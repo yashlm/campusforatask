d434ef2262f3167334fdd93a5469a3cf9464fc6a
Yash Chauhan <yashc22@iitk.ac.in>
2023-04-16 21:07:33 +0530

    campus

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <h1>CAMPUS FORA/YASH CHAUHAN</h1>
+        <button>Profile/Login</button>
+      </nav>
+      <header>
+        <div className="main">
+        <div className="ques">
+          <p><h2>Title</h2></p>
+          <p><h2>Content</h2></p>
+        </div>
+          <div className="ans">
+            <div className="button">
+              <button><img src="src/images/up.svg"></img></button>
+              <button><img src="src/images/down.svg"></img></button>
+              <button><img src="src/images/tick.svg"></img></button>
+              </div>
+            <h3>Top Answer</h3><p></p></div>
+        <div className="comments"><h3>Top Comments</h3><p>hjghjgh</p></div>
+    </div>
+      <aside>
+        <h3>Related Questions</h3>
+        </aside>
+        </header>
     </div>
   );
 }
